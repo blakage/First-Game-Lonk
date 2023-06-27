bbd44b7b6c39f3884f1584f241d1118af9cb8e3b
blakage <jondahl.thomas@gmail.com>
2023-06-27 10:56:01 -0500

    commit for camera
    added a camera movement feature that follows that game object

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public Transform creature; // Reference to the creature object
+    public float smoothTime = 0.3f; // Smoothing time for camera movement
+    public float zoomLevel = 5f; // Zoom level for the camera
+
+    private Vector3 velocity = Vector3.zero; // Velocity for smoothing
+
+    private void LateUpdate()
+    {
+        if (creature != null)
+        {
+            Vector3 targetPosition = new Vector3(creature.position.x, creature.position.y, transform.position.z);
+
+            // Smoothly move the camera towards the target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+
+    private void Start()
+    {
+        // Adjust the camera's orthographic size based on the zoom level
+        Camera.main.orthographicSize = zoomLevel;
+    }
+}
