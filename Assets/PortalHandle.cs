42b045f21bb70c481436e292801dfe0ee4a5bb06
blakage <jondahl.thomas@gmail.com>
2023-07-09 19:47:14 -0500

    Added portal to level 2
    Made it to where a portal will transfer you to level 2 only when you have 2 coins.

---
+++ Assets/PortalHandle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalHandle : MonoBehaviour
+{
+     public ImageFader imageFader;
+    // Start is called before the first frame update
+    public void PrintHello(){
+        Debug.Log("Hello!");
+    }
+
+    bool changingScenes = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<creature>() != null && Coin.score==2)
+        {
+            
+            ChangeScene("Level 2");
+
+        }
+    }
+
+    public void ChangeScene(string sceneName ){
+        if(changingScenes){
+            return;
+        }
+        changingScenes = true;
+        StartCoroutine(ChangeSceneRoutine());
+        IEnumerator ChangeSceneRoutine(){
+            imageFader.FadeToBlack();
+            yield return new WaitForSeconds(imageFader.fadeTime);
+            SceneManager.LoadScene(sceneName);
+            yield return null;
+        }
+        
+    }
+}
