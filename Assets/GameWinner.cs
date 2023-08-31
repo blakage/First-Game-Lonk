6940f3e8c1f3d78be4ae9babadeff4999f6bc599
blakage <jondahl.thomas@gmail.com>
2023-08-31 14:38:27 -0500

    slight change
    slight change

--- Assets/GameWinner.cs
+++ Assets/GameWinner.cs
@@ -9,6 +9,9 @@ public class GameWinner : MonoBehaviour
     public ImageFader imageFader;
     bool changingScenes = false;
     private AudioSource soundEffect;
+    
+
+    
     // Start is called before the first frame update
     void Start()
     {
